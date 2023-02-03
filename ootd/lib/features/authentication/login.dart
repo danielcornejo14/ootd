7c3ce79dab67c6db89cc91fe0f6472065b3337b5
Corne1401 <47864642+danielcornejo14@users.noreply.github.com>
2023-02-03 12:06:58 -0600

    Log in prototype form

--- ootd/lib/features/authentication/login.dart
+++ ootd/lib/features/authentication/login.dart
@@ -1,36 +1,90 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+  const Login({super.key});
 
   @override
-  _LoginState createState() => _LoginState();
+  State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
-
-  String email = '';
-
   @override
-  Widget build(BuildContext bluid) {
-
-    // ignore: prefer_const_constructors
-    return MaterialApp(
-      title: 'Login',
-      home: TextFormField(
-        decoration: const InputDecoration(
-          labelText: 'Email',
-          hintText: 'Enter your email',
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
         ),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter your email';
-          }
-          return null;
-        },
-        )
-    );
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
 
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Login'),
+                ),
+        
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                        
+                      if(value.contains('@') && value.contains('.') && value.length > 5){
+                        return null;
+                      }
+                        
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      
+                      hintText: 'Email',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.cyan, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      )
+                    ),
+                ),
+        
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.cyan, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => {
+                          
+                        },
+                        child: const Text('Login'),
+                      )
+                    ],
+                  )
+                ),
+              ],
+            ),
+          ),
+        ));
   }
-
 }
--- ootd/lib/main.dart
+++ ootd/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ootd/features/authentication/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'OOTD :3'),
+      home: const Login(),
     );
   }
 }
